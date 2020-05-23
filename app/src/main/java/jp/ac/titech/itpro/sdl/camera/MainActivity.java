4b5620fbb1004078ca511f3796f8fe6174502794
takashi <t.k.mk3.110@gmail.com>
2020-05-23 15:07:02 +0900

    beautify image

--- app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
@@ -3,7 +3,10 @@ package jp.ac.titech.itpro.sdl.camera;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -11,12 +14,17 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static int REQ_PHOTO = 1234;
+    private String currentPhotoPath = null;
     private Bitmap photoImage = null;
 
     @Override
@@ -28,7 +36,18 @@ public class MainActivity extends AppCompatActivity {
         photoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                File photoFile;
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException ex) {
+                    Toast.makeText(MainActivity.this, R.string.failed_to_create_image_file, Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                Uri photoURI = FileProvider.getUriForFile(MainActivity.this, "jp.ac.titech.itpro.sdl.camera", photoFile);
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+
                 PackageManager manager = getPackageManager();
                 List activities = manager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
                 if (!activities.isEmpty()) {
@@ -53,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(reqCode, resCode, data);
         if (reqCode == REQ_PHOTO) {
             if (resCode == RESULT_OK) {
-                photoImage = (Bitmap) data.getExtras().get("data");
+                photoImage = BitmapFactory.decodeFile(currentPhotoPath);
             }
         }
     }
@@ -62,5 +81,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         showPhoto();
+    }
+
+    private File createImageFile() throws IOException {
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                UUID.randomUUID().toString(),
+                ".jpg",
+                storageDir
+        );
+        image.deleteOnExit();
+
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
