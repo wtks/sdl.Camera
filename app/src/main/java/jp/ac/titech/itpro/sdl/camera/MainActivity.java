42006b7e14df61bd87fb87ca384f02203987b6a0
takashi <t.k.mk3.110@gmail.com>
2020-05-23 14:31:59 +0900

    impl camera

--- app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -27,9 +28,7 @@ public class MainActivity extends AppCompatActivity {
         photoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                // TODO: You should setup appropriate parameters for the intent
-
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 PackageManager manager = getPackageManager();
                 List activities = manager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
                 if (!activities.isEmpty()) {
@@ -54,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(reqCode, resCode, data);
         if (reqCode == REQ_PHOTO) {
             if (resCode == RESULT_OK) {
-                // TODO: You should implement the code that retrieve a bitmap image
+                photoImage = (Bitmap) data.getExtras().get("data");
             }
         }
     }
